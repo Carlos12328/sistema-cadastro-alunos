98f75be0b7e9e44a765cbb407c44d9577d802ffe
carlos <cadu.eduardo12328@gmail.com.br>
2026-06-04 19:03:55 -0300

    feat(auth): implementa protecao de rotas

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,27 +1,44 @@
 import { NavigationContainer } from '@react-navigation/native';
-
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
 import LoginScreen from '../screens/LoginScreen';
-
 import RegisterScreen from '../screens/RegisterScreen';
+import HomeScreen from '../screens/HomeScreen';
+import { useAuth } from '../context/AuthContext';
 
 const Stack = createNativeStackNavigator();
 
 export default function Routes() {
+
+  const { user, loading } = useAuth();
+
+  if (loading) {
+  return null;
+  }
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen
-          name="Login"
-          component={LoginScreen}
-        />
 
-        <Stack.Screen
-          name="Cadastro"
-          component={RegisterScreen}
-        />
-      </Stack.Navigator>
+      {user ? (
+        <>
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+          />
+        </>
+      ) : (
+        <>
+          <Stack.Screen
+            name="Login"
+            component={LoginScreen}
+          />
+          <Stack.Screen
+            name="Cadastro"
+            component={RegisterScreen}
+          />
+        </>
+      )}
+    </Stack.Navigator>
     </NavigationContainer>
   );
 }
